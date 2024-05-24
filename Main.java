8fe43653fc262c77d917409966abad574140ec68
João Pedro De Araújo Lemos <144943620+joaopedrolemos6@users.noreply.github.com>
2024-05-23 21:35:26 -0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        RefeitorioEscolar refeitorio = new RefeitorioEscolar();
-        CaixaRegistradora caixa = new CaixaRegistradora(refeitorio);
-
-        caixa.vender(1, 2); // Compra 2 hambúrgueres
-        caixa.vender(4, 1); // Compra 1 refrigerante
-    }
-}
